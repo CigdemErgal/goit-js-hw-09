de65998317acc11442d4fde2511e0d8c829dea54
Cigdem Ergal <cigdemtahtasiz@gmail.com>
2026-01-16 13:23:36 +0300

    ok

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,40 +1,42 @@
-const form = document.querySelector(`.feedback-form`);
-const STORAGE_KEY = `feedback-form-state`;
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
-let formData ={
-    email: ``,
-    message: ``,
-} 
+let formData = {
+  email: '',
+  message: '',
+};
 
 const savedData = localStorage.getItem(STORAGE_KEY);
-if (savedData){
-    try{
-    formData = JSON.parse(savedData);
 
-    form.elements.email.value = formData.email ?? ``;
-    form.elements.message.value = formData.message ?? ``;
-} catch (error) {
-    localStorage.removeItem(STORAGE_KEY);
-}
-}
-form.addEventListener(`input`, e => {
-    const { name, value } = e.target;
+if (savedData) {
+  formData = JSON.parse(savedData);
 
-    if (name !== `email` && name !== `message`) return;
-    formData[name] = value.trim();
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+
+form.addEventListener('input', e => {
+  const { name, value } = e.target;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
-form.addEventListener(`submit`, e => {
-    e.preventDefault();
 
-    if (formData.email === `` || formData.message === ``) {
-        alert(`Lütfen tüm alanları doldurun.`);
-        return;
-    }
-    console.log(formData);
-    form.reset();
-formData = {
-    email: ``,
-    message: ``,
-};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Lütfen tüm alanları doldurun.');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData = { email: '', message: '' };
+  form.reset();
 });
